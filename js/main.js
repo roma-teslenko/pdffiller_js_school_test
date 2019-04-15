bab9e8defa74814d6911550964a7345faca9bfc7
theromass <mr.romateslenko@gmail.com>
2019-04-15 18:15:09 +0300

    Add new files

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*____________________main_task_values_start____________________*/
+var params = {
+  lines:[
+    {
+      background: '#00F',
+      updateTime: 1000,
+    elements:[
+      {
+        background: '#00F',
+        width: 25
+      },
+      {
+        background: '#00F',
+        width: 50
+      },
+      {
+        background: '#00F',
+        width: 25
+      },
+    ]
+    }
+  ]
+}
+/*____________________main_task_values_end____________________*/
